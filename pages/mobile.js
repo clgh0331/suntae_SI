eca0975c923f6ab0d32fa7ce0aec70274718fefc
xodms1701 <iostream1701@gmail.com>
2021-03-06 01:23:02 +0900

    mobile nav

--- pages/mobile.js
+++ pages/mobile.js
@@ -6,6 +6,10 @@ import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
 class mobile extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      visibleNav: false,
+    };
   }
 
   render() {
@@ -18,7 +22,26 @@ class mobile extends React.Component {
               <LogoImage src={"mobileImages/logo.png"} />
               <LogoText>{COMPANY_NAME}</LogoText>
             </ImageArea>
-            <Menu src={"mobileImages/menu.png"} onClick={() => alert("매뉴")} />
+            <Menu
+              src={"mobileImages/menu.png"}
+              onClick={() => this.setState({ visibleNav: true })}
+            />
+            <Nav visible={this.state.visibleNav}>
+              <ImageArea>
+                <LogoImage src={"mobileImages/logo.png"} />
+                <LogoText>{COMPANY_NAME}</LogoText>
+              </ImageArea>
+              <NavClose onClick={() => this.setState({ visibleNav: false })} />
+              <NavWrapper>
+                <NavItem>Home</NavItem>
+                <NavItem>About US</NavItem>
+                <NavItem>Team</NavItem>
+                <NavItem>Token Distribution</NavItem>
+                <NavItem>Readmap</NavItem>
+                <NavItem>Contact</NavItem>
+                <NavItem>Disclaimer</NavItem>
+              </NavWrapper>
+            </Nav>
           </Header>
           <Content>
             <KskinIntroductionArea
@@ -57,6 +80,43 @@ class mobile extends React.Component {
 
 const COMPANY_NAME = "KSKIN";
 const MAIN_BACKGROUNDCOLOR = "rgba(235,177,163,1)";
+
+const Nav = styled.div`
+  display: ${(props) => (props.visible ? "block" : "none")};
+  position: absolute;
+  width: calc(100% - 60px);
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+  padding: 12px 30px;
+`;
+
+const NavClose = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  background-image: url("/mobileImages/Cross.png");
+  background-size: 20px;
+  width: 20px;
+  height: 20px;
+`;
+
+const NavWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  top: calc(50% - 260px);
+  left: calc(50% - 61px);
+`;
+
+const NavItem = styled.div`
+  color: white;
+  margin-bottom: 48px;
+  cursor: pointer;
+`;
 
 const Disclaimer = styled.img`
   height: 100%;
